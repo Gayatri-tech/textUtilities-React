3d304fdbac4f73f7029ed446377d04ec9dfadfd9
Gayu <gayatri28rawat@gmail.com>
2024-04-25 16:40:25 +0530

    added new functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,18 +1,65 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const handleUpClick = () => {
+  const [text, setText] = useState("");
+  const [undoStack, setUndoStack] = useState("[]");
+  const [redoStack, setRedoStack] = useState("[]");
+
+  const handleOnChange = (event) => {
+    let newText = event.target.value;
+    setUndoStack([...undoStack, text]);
+    setRedoStack([]);
+    setText(newText);
+  };
+
+  const handleUpperClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
   };
-  const handleLowClick = () => {
+
+  const handleLowerClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
   };
-  const handleOnChange = (event) => {
-    setText(event.target.value);
+
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
   };
-  const [text, setText] = useState("");
+
+  const handleTitleClick = () => {
+    let newText = text
+      .toLowerCase()
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+    setText(newText);
+  };
+
+  const handleSentenceClick = () => {
+    let sentence = text.toLowerCase().split(". ");
+    for (let i = 0; i < sentence.length; i++) {
+      sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+    }
+    let newText = sentence.join(". ");
+    setText(newText);
+  };
+
+  const handleUndoClick = () => {
+    if (undoStack.length > 0) {
+      const newText = redoStack.pop();
+      undoStack.push(text);
+      setText(newText);
+    }
+  };
+  const handleRedoClick = () => {
+    if (redoStack.length > 0) {
+      const newText = undoStack.pop();
+      redoStack.push(text);
+      setText(newText);
+    }
+  };
+
   return (
     <>
       <div className="container">
@@ -26,11 +73,41 @@ export default function TextForm(props) {
             onChange={handleOnChange}
           ></textarea>
         </div>
-        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
-          Convert to Uppercase
+        <button
+          className="btn btn-primary mx-1 my-1"
+          onClick={handleUpperClick}
+        >
+          UPPERCASE
         </button>
-        <button className="btn btn-primary mx-4" onClick={handleLowClick}>
-          Convert to Lowercase
+        <button
+          className="btn btn-primary mx-1 my-1"
+          onClick={handleLowerClick}
+        >
+          lowercase
+        </button>
+        <button
+          className="btn btn-primary mx-1 my-1"
+          onClick={handleTitleClick}
+        >
+          Title Case
+        </button>
+        <button
+          className="btn btn-primary mx-1 my-1"
+          onClick={handleSentenceClick}
+        >
+          Sentence case
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleUndoClick}>
+          Undo
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleRedoClick}>
+          Redo
+        </button>
+        <button
+          className="btn btn-primary mx-1 my-1"
+          onClick={handleClearClick}
+        >
+          Clear
         </button>
       </div>
       <div className="container my-3">
